7433226e03930088825145ab47ca88b1d95cc02e
Araf Hossain <arafh13@gmail.com>
2019-09-23 15:29:07 -0400

    Created and set getAuthInstance of Google Auth to state

--- client/src/Components/GoogleAuth.js
+++ client/src/Components/GoogleAuth.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: '779352453175-t29nj33tq44bh1rhs5ceadorapp34lit.apps.googleusercontent.com',
+        scope: 'email'
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({isSignedIn: this.auth.isSignedIn.get()})
+      })
+    })
+  }
+
   render() {
     return <div>Google-Auth</div>
   }
