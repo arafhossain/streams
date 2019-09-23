0dd300285ded56fea7eeddf8ef0319429fe5f7f1
Araf Hossain <arafh13@gmail.com>
2019-09-23 10:51:50 -0400

    Initialized client-side application

---
+++ client/src/Components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+let PageOne = () => {
+  return <div>PageOne</div>;
+}
+
+let PageTwo = () => {
+  return <div>PageTwo</div>;
+}
+
+let App = () => {
+  return (
+    <div>Hey!</div>
+  );
+};
+
+export default App;
