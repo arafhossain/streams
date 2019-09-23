8f9b760d55a3d733d972cb174dcc91a7b3b0e9bc
Araf Hossain <arafh13@gmail.com>
2019-09-23 11:55:11 -0400

    Imported stream components

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,32 +1,19 @@
-import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
-let PageOne = () => {
-  return (
-    <div>
-      PageOne
-      <Link to="/pagetwo">Navigate to Page Two</Link>
-    </div>
-  )
-}
 
-let PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <Link to="/">Navigate to Page One</Link>
-    </div>
-  )}
 
 let App = () => {
   return (
     <div>
-    <BrowserRouter>
-      <Route path="/" exact component={PageOne} />
-      <Route path="/pagetwo" component={PageTwo} />
-    </BrowserRouter>
+      <BrowserRouter></BrowserRouter>
     </div>
-    );
+  );
 };
 
 export default App;
