580c38c177fe160f742b2f52f36e8d1a55097fac
Araf Hossain <arafh13@gmail.com>
2019-09-23 16:12:38 -0400

    Created condition rendering for Signed In check

--- client/src/Components/GoogleAuth.js
+++ client/src/Components/GoogleAuth.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn : null};
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
@@ -9,12 +10,28 @@ class GoogleAuth extends React.Component {
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance();
         this.setState({isSignedIn: this.auth.isSignedIn.get()})
+        this.auth.isSignedIn.listen(this.onAuthChange);
       })
     })
   }
 
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get()})
+  }
+
+  renderAuthButton(){
+    console.log(this.state);
+    if(this.state.isSignedIn === null){
+      return <div>I don't know if we're signed in.</div>
+    } else if (this.state.isSignedIn){
+      return <div>I am signed in!</div>
+    } else {
+      return <div>I am not signed in!</div>
+    }
+  }
+
   render() {
-    return <div>Google-Auth</div>
+    return <div>{this.renderAuthButton()}</div>
   }
 }
 
