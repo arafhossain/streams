2c2c21d6c0acb6c3a8b03635a7610229edbce7f5
Araf Hossain <arafh13@gmail.com>
2019-09-26 13:16:09 -0400

    Created Server directory for streams

--- client/src/Components/streams/StreamCreate.js
+++ client/src/Components/streams/StreamCreate.js
@@ -4,23 +4,40 @@ import { Field, reduxForm } from "redux-form";
 class StreamCreate extends React.Component {
   renderInput(formProps) {
     return (
-      <input
-        onChange={formProps.input.onChange}
-        value={formProps.input.value}
-      />
-    );
+    <div>
+      <label>{formProps.label}</label>
+      <input {...formProps.input} />
+    </div>
+    )
+  }
+
+  onSubmit(formValues){
+    console.log(formValues);
   }
 
   render() {
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter Title"/>
+        <Field name="description" component={this.renderInput} label="Enter Description"/>
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
 }
 
+let validate = formValues => {
+  let errors = {};
+  if(!formValues.title){
+    errors.title = 'Title is required'
+  }
+  if(!formValues.description){
+    errors.description = 'Description is required'
+  }
+  return errors; 
+}
+
 export default reduxForm({
-  form: "streamCreate"
+  form: "streamCreate",
+  validate  
 })(StreamCreate);
